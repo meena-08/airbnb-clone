1ea3dedb2eceacb2d0f88d27114bd2e690f789bb
Meena <meena0252.cse19@chitkara.edu.in>
2022-01-04 14:32:41 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2020 Airbnb</p>
+            <p>FA4 Project by Nikame🥳</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
